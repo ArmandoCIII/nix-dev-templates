bdecfc7a59fb02394f1ec6ecafee2db4fa098d10
army castillo <arcastilloiii@gmail.com>
2026-03-02 11:03:02 -0500

    [docker image]: simple docker-image container template

--- containers/docker/nix/docker/build-docker-image.template.nix
+++ containers/docker/nix/docker/build-docker-image.template.nix
@@ -2,17 +2,13 @@
 { pkgs, ... }:
 
 # Generic Nix template for building Docker images
-pkgs.dockerTools.buildImage {
+pkgs.dockerTools.buildLayeredImage {
   name = "docker-image"; # Change to your image name
   tag = "latest";        # Change to your desired tag
   contents = [
-    # pkgs.curl
+    pkgs.curl
     # pkgs.git
     # pkgs.nodejs
     # Add more packages as needed
   ];
-  config = {
-    Cmd = [ "/bin/sh" ]; # Customize Docker CMD if needed
-    # Add more Docker config options here
-  };
 }
